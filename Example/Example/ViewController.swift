108646ea0d20b27b99ab3b9c0aa044716ee18005
lilidan <sgcy@yomac.local>
2015-11-19 21:45:28 +0800

    update

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +19,22 @@ class ViewController: UIViewController {
         let jsonObject = try! NSJSONSerialization.JSONObjectWithData(NSData(data: str.dataUsingEncoding(NSUTF8StringEncoding)!), options: NSJSONReadingOptions(rawValue: 0))
         print(jsonObject)
         let index:P.Index = P.parseJsonToObject(jsonObject, classType: P.Index.self)
+        models = index.data!.cases
    }
+    
+    var models:[P.Index.Data.Cases]!
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if models == nil{
+            return 0
+        }
+        return models.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell")
+        cell?.textLabel?.text = models[indexPath.row].title
+        return cell!
+    }
 }
 
